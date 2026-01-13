1380678dd60264c845fa11187840517d7fab197d
SeaOtocinclus <74376766+SeaOtocinclus@users.noreply.github.com>
2026-01-13 17:05:37 +0000

    Deploying to gh-pages from @ SeaOtocinclus/vrs@105c90db9eb29e9fd357278b5a90d11eeeb3e55c 🚀

--- doxygen/search/typedefs_1.js
+++ doxygen/search/typedefs_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['imagespectype_0',['ImageSpecType',['../namespacevrs_1_1datalayout__conventions.html#a21136a5b6de4f3ffa8e6d1d049c20c2d',1,'vrs::datalayout_conventions']]],
-  ['initcreatedthreadcallback_1',['InitCreatedThreadCallback',['../namespacevrs.html#a142c4b247fe86fb372c27f77d46e96b4',1,'vrs']]]
+  ['enableifstringconvertible_0',['EnableIfStringConvertible',['../namespacevrs.html#a285f77136ffb7d2a7655325ebd5c03e0',1,'vrs']]],
+  ['extras_1',['Extras',['../structvrs_1_1_file_spec.html#a76464a53f34587c44a663a3ba7e956dd',1,'vrs::FileSpec']]]
 ];
